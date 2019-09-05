b8079d3a5c4894333475e66f069aaa33e4f2b421
Swagata Chakraborty <44020073+swagatachakraborty@users.noreply.github.com>
2019-09-05 22:50:01 +0530

    use setter bean injection for getting custmer instance inside customer repository also

--- src/main/java/repository/HybernetCostomerRepositoryImpl.java
+++ src/main/java/repository/HybernetCostomerRepositoryImpl.java
@@ -6,14 +6,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class HybernetCostomerRepositoryImpl implements HybernetCostomerRepository {
+    private Customer customer;
+
     @Override
     public List<Customer> findAll() {
         List<Customer> customers = new ArrayList<>();
-        Customer customer = new Customer();
         customer.setFirstName("swagata");
         customer.setLastName("chakraborty");
         customers.add(customer);
         return customers;
+    }
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
     }
 }
 
